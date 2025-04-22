e84d9f7a02f216474350eb8e614361cf5565726e
Mike Dennis <mike@noreply.com>
2025-04-22 15:31:15 -0600

    AI fix for Issue #3

--- EmailApplication/emailapplication.client/src/index.tsx
+++ EmailApplication/emailapplication.client/src/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css'; // Optional: if you have global styles
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './context/AuthProvider'; // Import AuthProvider
+// import reportWebVitals from './reportWebVitals'; // Optional
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Ensure App is wrapped by AuthProvider */}
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
